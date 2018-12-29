e4f80d7994483dabcc5281b9d88227942883572c
Gustavo Leite <gustavo.jog@hotmail.com>
2018-12-29 17:23:47 -0200

    added retrofit instance

--- app/src/main/java/gustavo/acontece/data/RetrofitInstance.kt
+++ app/src/main/java/gustavo/acontece/data/RetrofitInstance.kt
@@ -1,2 +1,20 @@
 package gustavo.acontece.data
 
+import gustavo.acontece.R
+import gustavo.acontece.util.resourceprovider.ResourceProvider
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
+
+class RetrofitInstance {
+    companion object {
+        fun get(resourceProvider: ResourceProvider): Retrofit {
+            return retrofit2
+                .Retrofit
+                .Builder()
+                .baseUrl(resourceProvider.getString(R.string.API_BASE_URL))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
