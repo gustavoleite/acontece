6cd73a5e24d4a02cf9377690cbf49647baa2d8e4
Gustavo Leite <gustavo.jog@hotmail.com>
2018-12-31 11:59:42 -0200

    added layout of item list

--- app/src/main/java/gustavo/acontece/ui/home/HomeViewModel.kt
+++ app/src/main/java/gustavo/acontece/ui/home/HomeViewModel.kt
@@ -6,23 +6,26 @@ import gustavo.acontece.data.repository.EventRepositoryImpl
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(val eventRepository: EventRepositoryImpl): ViewModel() {
+
     val title = ObservableField<String>("Ola mundo")
-
     var compositeDisposable = CompositeDisposable()
-
 
     fun loadData() {
         eventRepository
             .fetchEvents()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { response ->
-                title.set(response[0].title)
-            }
+            .subscribeBy (
+                onError = {
+                    title.set("Ocorreu um erro na request")
+                },
+                onSuccess = { title.set(it[0].title)}
+            )
             .addTo(compositeDisposable)
     }
 
