bb538409ed73e794d806d8a1777eb16f0b1ebbdd
Gustavo Leite <gustavo.jog@hotmail.com>
2018-12-31 22:46:24 -0200

    added test

--- app/src/main/java/gustavo/acontece/utils/BindingAdapters.kt
+++ app/src/main/java/gustavo/acontece/utils/BindingAdapters.kt
@@ -3,8 +3,6 @@ package gustavo.acontece.utils
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
-import gustavo.acontece.R
-
 
 object BindingAdapters {
 
@@ -14,7 +12,6 @@ object BindingAdapters {
         Picasso
             .get()
             .load(imageUrl)
-            .placeholder(R.drawable.image_placeholder)
             .into(view)
     }
 }
