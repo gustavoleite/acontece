3b46ff18ef8f4f1cdb123572e04077e0c25ff7e6
“ilegra.gustavo” <ilegra0344@MacBook-Pro-Gustavo-Leite.local>
2019-01-02 11:30:11 -0200

    fixed repository test

--- app/src/main/java/gustavo/acontece/ui/eventdetail/PeopleListViewModel.kt
+++ app/src/main/java/gustavo/acontece/ui/eventdetail/PeopleListViewModel.kt
@@ -3,7 +3,7 @@ package gustavo.acontece.ui.eventdetail
 import android.arch.lifecycle.ViewModel
 import gustavo.acontece.data.entity.model.People
 
-class PeopleViewModel(people: People) : ViewModel() {
+class PeopleListViewModel(people: People) : ViewModel() {
     val name = people.name
     val selfie = people.picture
 }
--- app/src/test/java/gustavo/acontece/data/repository/EventRepositoryImplTest.kt
+++ app/src/test/java/gustavo/acontece/data/repository/EventRepositoryImplTest.kt
@@ -9,35 +9,49 @@ import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.reactivex.Single
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Test
 import java.util.*
 
 class EventRepositoryImplTest {
 
-    private lateinit var ceremonyRepositoryImpl: EventRepositoryImpl
+    private lateinit var eventRepositoryImpl: EventRepositoryImpl
 
     @MockK
     private lateinit var eventApi: EventApi
 
     private val cuponsItemOutputList = listOf(CuponOutput("1", 10, "1"))
     private val peopleItemOutputList = listOf(PeopleOutput("1", "Kelvin", "1", ""))
-    private val eventOutputList = listOf(EventOutput("1", "Event title", 1234,"",25.54, "-123", "-456", "Event description", cuponsItemOutputList, peopleItemOutputList))
+    private val eventOutputList = listOf(
+        EventOutput(
+            "1",
+            "title",
+            1234,
+            "",
+            25.54,
+            "-123",
+            "-456",
+            "Event description",
+            cuponsItemOutputList,
+            peopleItemOutputList
+        )
+    )
 
     @Before
     fun setup() {
         MockKAnnotations.init(this, relaxUnitFun = true)
+        eventRepositoryImpl = EventRepositoryImpl(eventApi)
     }
 
     @Test
     fun `fetch events`() {
-        every { eventApi.getAllEvents() } returns Single.just(
+        every {
+            eventApi.getAllEvents()
+        } returns Single.just(
             eventOutputList
         )
-        ceremonyRepositoryImpl = EventRepositoryImpl(eventApi)
-        val expectedValue = Single.just(listOf(EventPreview("1", "Event", "", 25.54, Calendar.getInstance())))
-        assertThat(ceremonyRepositoryImpl.fetchEvents(), `is`(expectedValue))
+        val expectedValue =
+            listOf(EventPreview("1", "title", "", 25.54, Calendar.getInstance().apply { timeInMillis = 1234 }))
+        eventRepositoryImpl.fetchEvents().test().assertValue(expectedValue)
     }
 }
