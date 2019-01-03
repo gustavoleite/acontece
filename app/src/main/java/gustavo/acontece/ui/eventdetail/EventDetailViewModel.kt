5eec378502948d2701c81455ce23a5e5b78861f1
Gustavo Leite <gustavo.jog@hotmail.com>
2019-01-02 23:15:25 -0200

    added date and price to top

--- app/src/main/java/gustavo/acontece/ui/eventdetail/EventDetailViewModel.kt
+++ app/src/main/java/gustavo/acontece/ui/eventdetail/EventDetailViewModel.kt
@@ -6,6 +6,8 @@ import gustavo.acontece.R
 import gustavo.acontece.data.entity.model.EventDetail
 import gustavo.acontece.data.repository.EventRepositoryImpl
 import gustavo.acontece.ui.base.BaseViewModel
+import gustavo.acontece.utils.extensions.toBrCurrency
+import gustavo.acontece.utils.extensions.toDayMonthYear
 import gustavo.acontece.utils.resource.ResourceProvider
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.addTo
@@ -13,20 +15,27 @@ import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class EventDetailViewModel  @Inject constructor(val eventRepository: EventRepositoryImpl, resourceProvider: ResourceProvider): BaseViewModel() {
+class EventDetailViewModel @Inject constructor(
+    val eventRepository: EventRepositoryImpl,
+    resourceProvider: ResourceProvider
+) : BaseViewModel() {
 
     val event = MutableLiveData<EventDetail>()
     val image = ObservableField<String>()
     val description = ObservableField<String>()
+    val cupon = ObservableField<String>()
+    val date = ObservableField<String>()
+    val price = ObservableField<String>()
     val peopleSection = resourceProvider.getString(R.string.events_detail_organizers)
     val locationSection = resourceProvider.getString(R.string.events_detail_location)
+    val cuponSection = resourceProvider.getString(R.string.events_detail_cupom)
 
     fun loadData(eventId: String) {
         eventRepository
             .fetchEventDetail(eventId)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribeBy (
+            .subscribeBy(
                 onSuccess = {
                     setData(it)
                 }
@@ -38,5 +47,15 @@ class EventDetailViewModel  @Inject constructor(val eventRepository: EventReposi
         event.value = it
         description.set(it?.description)
         image.set(it?.image)
+        cupon.set(calculateCupon(it))
+        date.set(it?.date?.toDayMonthYear())
+        price.set(it?.price?.toBrCurrency())
+    }
+
+    private fun calculateCupon(eventDetail: EventDetail?): String {
+        eventDetail?.let {
+            return (eventDetail.price - eventDetail.cupons.first().discount).toBrCurrency()
+        }
+        return 0.0.toBrCurrency()
     }
 }
