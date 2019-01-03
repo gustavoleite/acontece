80e118f29b36800d2dcd4b16ae2fdd731687fc88
Gustavo Leite <gustavo.jog@hotmail.com>
2019-01-03 20:15:01 -0200

    added checkin view model

--- app/src/main/java/gustavo/acontece/ui/checkin/CheckinViewModel.kt
+++ app/src/main/java/gustavo/acontece/ui/checkin/CheckinViewModel.kt
@@ -1,2 +1,28 @@
 package gustavo.acontece.ui.checkin
 
+import android.databinding.ObservableField
+import gustavo.acontece.data.repository.EventRepository
+import gustavo.acontece.ui.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+class CheckinViewModel @Inject constructor(val eventRepository: EventRepository) : BaseViewModel() {
+
+    val name = ObservableField<String>()
+    val email = ObservableField<String>()
+    lateinit var eventId: String
+
+    fun onCheckinPressed() {
+        eventRepository
+            .makeCheckin(eventId, "gustavo", "")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy {
+
+            }
+            .addTo(compositeDisposable)
+    }
+}
